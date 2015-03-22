a262b31633b1f7a027fb10d1e1684f0fbc060b8e
Seisyo Hsu <jo-seisho@MacBook-Pro-2.local>
2015-03-22 15:05:36 +0800

    There have the THSRstutime's simple interface.

--- root.js
+++ root.js
@@ -1,4 +1,7 @@
 angular.module("root",[])
 	.controller("index",["$scope",function($scope){
-		$scope.message = "Hello World";
+		$scope.message="THSR student's ticket price search system";
+		$scope.depstation;
+		$scope.arrstation;
+		$scope.deptime;
 	}]);
