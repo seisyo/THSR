a4576f7ee332c2cf97f84f319aaef1a1c6844a5f
Seisyo Hsu <jo-seisho@ip149-65.wlan.ntust.edu.tw>
2015-03-23 10:12:01 +0800

    Bold italic underline ng-class, but it can't work.

--- root.js
+++ root.js
@@ -3,5 +3,9 @@ angular.module("root",[])
 		$scope.message="THSR student's ticket price search system";
 		$scope.depstation;
 		$scope.arrstation;
+		$scope.depdate;
 		$scope.deptime;
+		$scope.isBold=function(){return true;};
+		$scope.isItalic=function(){return true;};
+		$scope.isUnderline=function(){return true;};
 	}]);
