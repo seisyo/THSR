42dab2a6729aaa5c9f77fd8b40b7d7db422abfed
Seisyo Hsu <jo-seisho@MacBook-Pro-2.local>
2015-03-29 14:51:57 +0800

    add the angularjs's jsonp function , but it can't receive the data from my flask api. Idon't know where the error came from, I guess it may has problem in api(?

--- root.js
+++ root.js
@@ -1,11 +1,34 @@
 angular.module("root",[])
-	.controller("index",["$scope",function($scope){
-		$scope.message="THSR student's ticket price search system";
-		$scope.depstation;
-		$scope.arrstation;
-		$scope.depdate;
-		$scope.deptime;
-		$scope.isBold=function(){return true;};
-		$scope.isItalic=function(){return true;};
-		$scope.isUnderline=function(){return true;};
-	}]);
+.controller("index", ["$scope", "$http", function($scope, $http){
+
+    $scope.depclick = function(){
+        if($scope.depstation != "undefined" && $scope.arrstation != "undefined" && $scope.depdate != "undefined" && $scope.deptime != "undefined"){
+            console.log($scope.depstation);
+            console.log($scope.arrstation);
+            console.log($scope.depdate);
+            console.log($scope.deptime);
+                // console.log($http);
+                // $http.get('http://127.0.0.1:5000/searchtrain', {
+                //     depsta: $scope.depstation, 
+                //     arrsta: $scope.arrstation, 
+                //     date: $scope.depdate, 
+                //     time: $scope.deptime
+                // }).success(function(data){
+                //     console.log(data);
+                // });
+            var responsePromise = $http.jsonp( "http://127.0.0.1:5000/searchtrain?callback=JSON_CALLBACK", {
+                depsta: $scope.depstation | String, 
+                arrsta: $scope.arrstation | String, 
+                date: $scope.depdate | String, 
+                time: $scope.deptime | String
+             });
+
+            responsePromise.success(function(data) {
+                console.log(data);
+    // do something with the returned JavaScript object
+    // ( in the "data" parameter ).
+            });
+
+}
+};
+}]);
